4a7950235437c9647c20962500ae7740e6c52e11
MilosMladenovicWork <milos.mladenovic.work@gmail.com>
2023-10-14 15:33:41 +0200

    fix(team): import SEO component

--- src/templates/team.js
+++ src/templates/team.js
@@ -1,6 +1,7 @@
 import { graphql } from 'gatsby';
 import React from 'react';
 import Layout from '../components/Layout';
+import SEO from '../components/SEO';
 
 const Team = ({ data }) => {
   const { title } = data.markdownRemark.frontmatter;
